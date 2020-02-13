57d0c82fb841467ca05be1d95e7292ec65b7b5af
Aiden Liu <aiden.lx@outlook.com>
2020-02-13 18:22:32 +0800

    Initial Commit

---
+++ mnanki/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mnanki
+//
+//  Created by Aiden Liu on 2020/2/13.
+//  Copyright © 2020 Aiden Liu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
